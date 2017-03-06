e517dc56c8becb63554ba0c462466954ba372caa
pablonchu <paublonchugames@gmail.com>
2017-03-06 17:59:44 +0100

    Acabado Test del cliente salvo el bugged

---
+++ src/test/java/AplicacionTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by al341845 on 06/03/2017.
+ */
+public class AplicacionTest {
+}
--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -11,35 +11,39 @@ import static org.junit.Assert.assertEquals;
  * Este Test comprueba los métodos publicos de la clase Cliente.
  */
 
-public class ClienteTest{
+public class ClienteTest {
 
     private static String nombre, apellido, NIF, provincia, poblacion;
     private static Direccion direccion;
     private static Cliente empresa, particular;
     private static LocalDateTime fecha;
-    private static Tarifa tarifa = new Tarifa(20);
-    private LinkedList<Llamada> listaLLamadas = new LinkedList<>();
+    private static Tarifa tarifa = new Tarifa (20);
+    private LinkedList<Llamada> listaLLamadas = new LinkedList<> ();
     private static Llamada llam1;
-
+    private static Llamada llam2;
+    private static Factura factura1;
     @BeforeClass
-    public static void init(){
+    public static void init() {
 
-        GeneradorDatosINE gen = new GeneradorDatosINE();
-        nombre = gen.getNombre();
-        apellido = gen.getApellido();
-        NIF = gen.getNIF();
-        provincia = gen.getProvincia();
-        poblacion = gen.getPoblacion(provincia);
-        direccion = new Direccion("12006",provincia,poblacion);
-        particular = new Particular(nombre,apellido,NIF,direccion,fecha,tarifa);
-        empresa = new Empresa(nombre,NIF,direccion,fecha,tarifa);
-        fecha = fecha.now();
-        llam1 = new Llamada("964048351",20,fecha.minusHours (1));
+        GeneradorDatosINE gen = new GeneradorDatosINE ();
+        nombre = gen.getNombre ();
+        apellido = gen.getApellido ();
+        NIF = gen.getNIF ();
+        provincia = gen.getProvincia ();
+        poblacion = gen.getPoblacion (provincia);
+        direccion = new Direccion ("12006", provincia, poblacion);
+        particular = new Particular (nombre, apellido, NIF, direccion, fecha, tarifa);
+        empresa = new Empresa (nombre, NIF, direccion, fecha, tarifa);
+        fecha = fecha.now ();
+        llam1 = new Llamada ("964048351", 20, fecha.minusHours (1));
+        llam2 = new Llamada ("943882182", 15, fecha.minusDays (1));
+        factura1 = new Factura(fecha,fecha.minusMinutes(30),
+                fecha.minusMinutes(10),particular,tarifa);
     }
 
   /*  @Test
     public void getLlamadaPeriodo(){
-        Llamada llam2 = new Llamada("943882182",15,fecha.minusDays(1));
+
         LinkedList<Llamada> original = particular.getListall();
         original.add(llam1);
         original.add(llam2);
@@ -58,59 +62,76 @@ public class ClienteTest{
     @Test
 
 
-    public void testSwapTarifa(){
-        assertEquals(particular.getTarifa(),tarifa);
-        Tarifa old = particular.getTarifa();
-        Tarifa otra = new Tarifa(40);
+    public void testSwapTarifa() {
+        assertEquals (particular.getTarifa (), tarifa);
+        Tarifa old = particular.getTarifa ();
+        Tarifa otra = new Tarifa (40);
 
-        particular.swpTarifa(otra);
-        assertEquals(otra,particular.getTarifa());
-        particular.swpTarifa(old);
-
-
-    }
-
-    @Test
-
-    public void testAddLlamada(){
-        LinkedList<Llamada> original = particular.getListall();
-        original.add(llam1);
-        particular.addLlamada(llam1);
-        assertEquals(original,particular.getListall());
+        particular.swpTarifa (otra);
+        assertEquals (otra, particular.getTarifa ());
+        particular.swpTarifa (old);
     }
 
 
     @Test
-    public void testGetNif(){
+    public void testAddLlamada() {
+        LinkedList<Llamada> original = particular.getListall ();
+        original.add (llam1);
+        particular.addLlamada (llam1);
+        assertEquals (original, particular.getListall ());
+    }
 
-        assertEquals(particular.getNif(),NIF);
-        assertEquals(empresa.getNif(),NIF);
+
+    @Test
+    public void testGetNif() {
+
+        assertEquals (particular.getNif (), NIF);
+        assertEquals (empresa.getNif (), NIF);
 
     }
 
     @Test
-    public void testGetTarifa(){
-        assertEquals(particular.getTarifa(),tarifa);
-        assertEquals(empresa.getTarifa(),tarifa);
+    public void testGetListall() {
+        LinkedList<Llamada> esperado = new LinkedList<Llamada> ();
+        esperado.add (llam1);
+        particular.addLlamada (llam1);
+        LinkedList<Llamada> resultado = particular.getListall ();
+        assertEquals (esperado, resultado);
+    }
+
+    @Test
+    public void testGetTarifa() {
+        assertEquals (particular.getTarifa (), tarifa);
+        assertEquals (empresa.getTarifa (), tarifa);
     }
 
     @Test
 
-    public void testAddFactura(){
-        LinkedList<Factura> original = particular.getListafac();
-        Factura newFactura = new Factura(fecha,fecha.minusMinutes(30),
-                fecha.minusMinutes(10),particular,tarifa);
-        original.add(newFactura);
-        particular.addFactura(newFactura);
-        assertEquals(original,particular.getListafac());
+    public void testAddFactura() {
+        LinkedList<Factura> original = particular.getListafac ();
+        Factura newFactura = new Factura (fecha, fecha.minusMinutes (30),
+                fecha.minusMinutes (10), particular, tarifa);
+        original.add (newFactura);
+        particular.addFactura (newFactura);
+        assertEquals (original, particular.getListafac ());
 
     }
+
+    @Test
+    public void testGetListaFac() {
+        LinkedList<Factura> esperado = new LinkedList<Factura>();
+        esperado.add (factura1);
+        particular.addFactura (factura1);
+        LinkedList<Factura> original = particular.getListafac ();
+        assertEquals (original,esperado);
+    }
+
 
 
     @After
-    public void remove(){
-        particular.getListall().clear();
-        particular.getListafac().clear();
+    public void remove() {
+        particular.getListall ().clear ();
+        particular.getListafac ().clear ();
 
     }
 
