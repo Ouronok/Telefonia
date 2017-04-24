303bf88036ac7752103baaf47cc295aee8bb3164
Ouronok <ouronok@gmail.com>
2017-04-24 22:28:25 +0200

    bugfixes

--- src/main/java/tarifas/TarifaDomingo.java
+++ src/main/java/tarifas/TarifaDomingo.java
@@ -6,14 +6,13 @@ import pago.Llamada;
 import java.time.DayOfWeek;
 
 public class TarifaDomingo extends TarifaEspecial {
-//        private Tarifa tarifa;
-        public TarifaDomingo(Tarifa tarifa){
-            super(0, tarifa);
-        }
+    public TarifaDomingo(Tarifa tarifa) {
+        super(0, tarifa);
+    }
 
     @Override
     public double precioLlamada(Llamada llamada) {
-        if(llamada.getFecha().getDayOfWeek() == DayOfWeek.SUNDAY)
+        if (llamada.getFecha().getDayOfWeek() == DayOfWeek.SUNDAY)
             return 0;
         else return getRecubierta().precioLlamada(llamada);
     }
@@ -22,9 +21,4 @@ public class TarifaDomingo extends TarifaEspecial {
     public double getPrecio() {
         return 0;
     }
-
-//    @Override
-//        public double getPrecio(){
-//            return Math.min(tarifa.getPrecio(),super.precio);
-//        }
 }
