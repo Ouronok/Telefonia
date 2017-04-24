a1610ed39918f0ca3c89cc6fe3f8f94359bd423a
Ouronok <ouronok@gmail.com>
2017-04-24 21:18:54 +0200

    bugfixes

--- src/main/java/aplicacion/FactoriaTarifas.java
+++ src/main/java/aplicacion/FactoriaTarifas.java
@@ -1,6 +1,7 @@
 package aplicacion;
 
 
+import org.jetbrains.annotations.NotNull;
 import tarifas.Tarifa;
 import tarifas.TarifaBasica;
 import tarifas.TarifaDomingo;
@@ -11,14 +12,17 @@ import tarifas.TarifaTardes;
 public class FactoriaTarifas {
 
 
+    @NotNull
     public static TarifaBasica creaTarifa(Double precio) {
         return new TarifaBasica(precio);
     }
 
+    @NotNull
     public static TarifaDomingo creaDomingo(Tarifa tarifa) {
         return new TarifaDomingo(tarifa);
     }
 
+    @NotNull
     public static TarifaTardes creaTardes(Tarifa tarifa) {
         return new TarifaTardes(tarifa, (double) 10);
     }
